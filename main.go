1d910690289403e05c5e267f961b5e2366787d90
MasaKu <masaki_kuyama@yahoo.co.jp>
2026-03-29 22:09:33 +0900

    fix timezone

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
-	now := time.Now().Format("2006-01-02 15:04:05")
+
+	// 日本標準時 (JST) のロケーションを取得
+	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+	now := time.Now().In(jst).Format("2006-01-02 15:04:05")
 	fmt.Printf("[%s] GitHub Actions: Goプログラムが正常に実行されました。\n", now)
 }
