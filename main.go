d3f4464735e29c830395d86c6a92ecea5c6cb08b
MasaKu <masaki_kuyama@yahoo.co.jp>
2026-03-29 21:39:02 +0900

    create cron.yaml

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
+	now := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Printf("[%s] GitHub Actions: Goプログラムが正常に実行されました。\n", now)
 }
